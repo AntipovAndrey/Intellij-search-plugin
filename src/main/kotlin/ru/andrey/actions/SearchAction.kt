374b7a48cbefb9e7ef69af52291f8746677ed40e
AntipovAndrey <antipov11.11@gmail.com>
2018-12-15 11:14:55 +0300

    action stub

---
+++ src/main/kotlin/ru/andrey/actions/SearchAction.kt
@@ -0,0 +1,11 @@
+package ru.andrey.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class SearchAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
