9758dda3c3b49127d6f4b604e1b7af6556d0f859
AntipovAndrey <antipov11.11@gmail.com>
2018-12-15 17:30:02 +0300

    use engine provided by settings class

--- src/main/kotlin/ru/andrey/actions/SearchAction.kt
+++ src/main/kotlin/ru/andrey/actions/SearchAction.kt
@@ -4,12 +4,14 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.editor.Editor
-import ru.andrey.search.GoogleSearcher
+import ru.andrey.di.Di
+import ru.andrey.search.Searcher
 
 class SearchAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val caretModel = e.getData<Editor>(LangDataKeys.EDITOR)!!.caretModel
-        GoogleSearcher().search(caretModel.currentCaret.selectedText)
+        Di.getInstance(Searcher::class.java)
+                .search(caretModel.currentCaret.selectedText)
     }
 }
