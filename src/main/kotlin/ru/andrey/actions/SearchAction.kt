91fa480776d2066a470806177be114da2adc11ec
AntipovAndrey <antipov11.11@gmail.com>
2018-12-18 17:42:35 +0300

    disable action if nothing to copy

--- src/main/kotlin/ru/andrey/actions/SearchAction.kt
+++ src/main/kotlin/ru/andrey/actions/SearchAction.kt
@@ -3,9 +3,9 @@ package ru.andrey.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.PlatformDataKeys.COPY_PROVIDER
 import com.intellij.openapi.project.DumbAware
 import ru.andrey.di.Di
-import ru.andrey.search.Searcher
 import ru.andrey.settings.SearchEngine
 import ru.andrey.settings.Settings
 
@@ -16,12 +16,24 @@ class SearchAction : AnAction(), DumbAware {
     override fun actionPerformed(e: AnActionEvent) {
         e.getData(CommonDataKeys.EDITOR)?.let {
             val term = it.caretModel.currentCaret.selectedText
-            Di[Searcher::class].search(term)
+            settings.searcher.search(term)
         }
     }
 
     override fun update(event: AnActionEvent) {
         updateActionAppearance(settings.searchEngine)
+        disableIfNothingToCopy(event)
+    }
+
+    private fun disableIfNothingToCopy(event: AnActionEvent) {
+        val presentation = event.presentation
+        val dataContext = event.dataContext
+        val provider = COPY_PROVIDER.getData(dataContext)
+        val available = provider != null &&
+                provider.isCopyEnabled(dataContext) &&
+                provider.isCopyVisible(dataContext)
+        presentation.isEnabled = available
+        presentation.isVisible = available
     }
 
     private fun updateActionAppearance(engine: SearchEngine) {
